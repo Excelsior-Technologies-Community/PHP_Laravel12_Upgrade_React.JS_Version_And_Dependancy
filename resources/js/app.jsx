970cd92a3d02ca5308c0e6f15d36000ec802203b
patelnishit1012-sys <patelnishit1012@gmail.com>
2026-01-05 12:05:37 +0530

    Initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div style={{ padding: 20 }}>
+            <h2>React 16 is working in Laravel 12 </h2>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
